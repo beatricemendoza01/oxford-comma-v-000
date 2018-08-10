9d2b3dac3540d704357718785a9155e9c1128a30
Beatrice Mendoza <mendoza@nyu.edu>
2018-08-10 03:19:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,7 @@
 def oxford_comma(array)
   if array.length != 1
-    array.join(", ") << ", and #{array.pop}"
+    last = ", and #{array.pop}"
+    array.join(", ") << last
   end
   array.join
 end
